bd1d41c19b6b2d326375d1a500877e7342358d86
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2021-12-07 16:07:42 -0500

    Update and rename Unsure.java to PrimeNumbers.java

--- Assignments/PrimeNumbers.java
+++ Assignments/PrimeNumbers.java
@@ -1,11 +1,9 @@
 import java.util.Scanner;
 
-public class Testing {
+public class PrimeNumbers {
 
 public static void main(String[] args) {
 
-	// Nesta Fletcher
-	// 9/30/2021
 	// Horstmann Chapter 4, Project P4.18
 	// This program will take a user entered number and return all prime numbers (divisible by only 1 and itself) up to that number entered
 	
