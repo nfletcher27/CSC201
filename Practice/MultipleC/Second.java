847addd447d16fbdff5e43d268a69a557c990e3e
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2021-12-10 23:03:10 -0500

    Create Second.java

---
+++ Practice/MultipleC/Second.java
@@ -0,0 +1,8 @@
+class Second {
+  public static void main(String[] args) {
+    Main myCar = new Main();     // Create a myCar object
+    myCar.fullThrottle();        // Call the fullThrottle() method
+    mycar.Speed(200);            // Call the speed() method
+    // Output should be The car is going as fast as it can! 200
+  }
+}
