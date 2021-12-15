f743c848172cc1dfb811f8b9ef4429247f286b7d
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2021-12-15 00:32:51 -0500

    Update LinkedList.java

--- Practice/LinkedList.java
+++ Practice/LinkedList.java
@@ -1,3 +1,28 @@
 // LinkedList is almost identical to ArrayList
 
 import java.util.LinkedList;
+
+public class Main {
+  public static void main(String[] args) {
+    LinkedList<String> cars = new LinkedList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Benz");
+    cars.add("RollsRoyce");
+    System.out.println(cars);
+  }
+}
+
+// LinkedList has all the same methods of ArrayList as they both implement the List interface.
+// LinkedList stores its items in containers, and all the containers are linked
+// An ArrayList is used for storing and accessing data, a LinkedList is for manipulating data
+
+// list of useful methods for LinkedList
+
+addFirst()  // adds an item to the beginning of the list
+addLast()  // self explanatory /\
+removeFirst() // removes item at beginning of list
+removeLast() // end of list
+getFirst() // gets first item
+getLast() // same
+      
