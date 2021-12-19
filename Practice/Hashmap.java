d8eb5763a6c5b876b5a9d7c2bc22ba6b0e583356
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2021-12-18 22:56:17 -0500

    Update Hashmap.java

--- Practice/Hashmap.java
+++ Practice/Hashmap.java
@@ -1,1 +1,19 @@
 // A Hashmap stores items as keys and values, and you access them with an index of another type (e.g. String)
+
+import java.util.Hashmap;
+
+HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+// add items likeso
+public class Main {
+  public static void main(String[] args) {
+    // Create a HashMap object
+    HashMap<String, String> capitalCities = new HashMap<String, String>();
+    
+    capitalCities.put("England", "London");
+    capitalCities.put("Germany", "Berlin");
+    capitalCities.put("Norway", "Oslo");
+    capitalCities.put("USA", "DC");
+    System.out.println(capitalCities);
+  }
+}
