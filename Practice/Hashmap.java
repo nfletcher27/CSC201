da67e106eba8557370a8c79dd23f263db1a2d5f3
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2021-12-19 00:25:35 -0500

    Update Hashmap.java

--- Practice/Hashmap.java
+++ Practice/Hashmap.java
@@ -17,3 +17,51 @@ public class Main {
     System.out.println(capitalCities);
   }
 }
+
+// access an item
+capitalCities.get("England"); // will return London
+
+// remove an item
+capitalCities.remove("England");
+
+// clear all items
+capitalCities.clear();
+
+// return how many items 
+capitalCities.size();
+
+// use a for-each loop to loop through
+// you can use keySet() method for only keys, and values() respectively
+for (String i : capitalCities.keySet()) {
+  System.out.println(i);
+}
+
+for (String i : capitalCities.values()) {
+  System.out.println(i);
+}
+
+for (String i : capitalCities.values()) {
+  System.in.println("key: " + i + " value " + capitalCities.get(i));
+}
+
+// Keys and values are typically objects. Strings are objects, but for other types such as ints, you must specify the wrapper class.
+// HashMap set for String keys and Integer values
+import java.util.HashMap;
+
+public class Main {
+  public static void main(String[] args) {
+    
+    // Create HashMap object called people
+    HashMap<String, Integer> people = new HashMap<String, Integer>();
+    
+    // Add keys and values (Name, Age)
+    people.put("John", 32);
+    people.put("Steve", 30);
+    people.put("Angel", 33);
+    
+    for (String i : people.keySet()) {
+      System.out.println("key: " + i + " value: " + people.get(i));
+    }
+  }
+}
+
