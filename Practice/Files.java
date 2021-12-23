a9aba07def5bd8fb52195d1177ffddc29d8756de
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2021-12-22 22:13:39 -0500

    Create Files.java

---
+++ Practice/Files.java
@@ -0,0 +1,12 @@
+// To create a file, create an object of type File and specify the filename or directory
+File myObj = new File("filename.txt");
+
+// There are plenty of useful methods such as:
+canRead()
+  createNewFile()
+  delete()
+  exists()
+  getName()
+  list()
+  mkdir()
+  
