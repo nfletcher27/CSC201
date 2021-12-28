f0d82416719ebc9c18b583b279d00a1e29c12554
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2021-12-28 00:10:55 -0500

    Update Cat.java

--- Projects/Dog/Cat.java
+++ Projects/Dog/Cat.java
@@ -6,9 +6,13 @@ public class Cat extends Dog {
 		super(name, age);
 		this.food = food;
 	}
+	public Cat(String name, int age) {
+		super(name, age);
+	}
 	
 	public void talk() {
 		System.out.println("I am " + this.name + " and I am " + this.age + " years old" + " and I'm " + this.food + " hungry");
 	}
 	
 }
+
