872164c17eb269232875c42713ed4940c1f3d7b1
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2021-12-28 00:10:18 -0500

    Update Main.java

--- Projects/Dog/Main.java
+++ Projects/Dog/Main.java
@@ -2,10 +2,14 @@ public class Main {
 	public static void main(String[] args) {
 	
 		Dog mark = new Dog("mark", 5);
+		Cat sammy = new Cat("Sammy", 6, 20);
+		Cat wilson = new Cat("Wilson", 22);
 		mark.talk();
 		mark.setAge(11);
 		mark.talk();
 		mark.add2();
 		mark.talk();
+		sammy.talk();
+		wilson.talk();
 	}
 }
