9adc76b64e99739aef79f0a9df50a2348433c053
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2021-12-27 21:39:20 -0500

    Create Dog.java

---
+++ Projects/Dog/Dog.java
@@ -0,0 +1,26 @@
+public class Dog {
+
+	private String name;
+	private int age;
+	
+	public Dog(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public void talk() {
+		System.out.println("I am " + this.name + " and I am " + this.age + " years old");
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge (int age) {
+		this.age = age;
+	}
+	
+	public void add2() {
+		this.age = age + 2;
+	}
+}
