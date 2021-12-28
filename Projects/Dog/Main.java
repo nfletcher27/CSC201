c9463112220ffb9adf859a6367e133ec6b52c667
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2021-12-27 21:38:59 -0500

    Create Main.java

---
+++ Projects/Dog/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+	
+		Dog mark = new Dog("mark", 5);
+		mark.talk();
+		mark.setAge(11);
+		mark.talk();
+		mark.add2();
+		mark.talk();
+	}
+}
