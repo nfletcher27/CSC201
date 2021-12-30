02ea5d6b074f2197b45cfcf6c92147d655312b43
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2021-12-29 19:45:38 -0500

    Create Stacks.java

---
+++ Practice/Stacks.java
@@ -0,0 +1,36 @@
+public class StacksAreFun {
+  public static void main(String[] args) {
+    
+    Stack<String> games = new Stack<String>();
+    
+    games.add("Minecraft");
+    games.add("Terraria");
+    games.add("GTA V");
+    games.add("Roblox");
+    games.add("Counter Strike");
+    games.add("Team Fortress 2");
+    
+    System.out.println(games.pop());
+    System.out.println(games.peek());
+    System.out.println(games);
+  }
+}
+
+public class Legos {
+  public static void main(String [] args) {
+    
+    // Y
+    // B
+    // R
+    
+    Stack<Character> tower = new Stack<Character>();
+    tower.add('R');
+    tower.add('B');
+    tower.add('Y');
+    
+    System.out.println(tower.pop());
+    System.out.println(tower.contains('B');
+    System.out.println(tower.indexOf(0);
+    }
+}    
+    
