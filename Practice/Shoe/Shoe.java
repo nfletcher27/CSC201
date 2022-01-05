ada94e5342fd86b5851ca896f9b430678a44cb6d
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-01-04 22:58:51 -0500

    Create Shoe.java

---
+++ Practice/Shoe/Shoe.java
@@ -0,0 +1,15 @@
+public class Shoe {
+
+	final public String brand;
+	final public double size;
+	
+	public Shoe(String brand, double size) {
+		this.brand = brand;
+		this.size = size;
+	}
+	
+	
+	
+	
+}
+
