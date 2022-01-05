facbfa0d8fb4e11cc3341060b62f40d140c56b8d
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-01-04 22:59:34 -0500

    Create Walking.java

---
+++ Practice/Shoe/Walking.java
@@ -0,0 +1,11 @@
+public class Walking extends Shoe {
+
+	final public boolean goreTex;
+	
+	public Walking(String brand, double size, boolean goreTex) {
+		super(brand, size);
+		this.goreTex = goreTex;
+		
+	}
+
+}
