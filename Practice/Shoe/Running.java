81cb4da0c795d94e24e920dc9c27ad97571e25d9
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-01-04 22:59:07 -0500

    Create Running.java

---
+++ Practice/Shoe/Running.java
@@ -0,0 +1,10 @@
+public class Running extends Shoe {
+	
+	final public double weight;
+
+	public Running(String brand, double size, double weight) {
+		super(brand, size);
+		this.weight = weight;
+	}
+
+}
