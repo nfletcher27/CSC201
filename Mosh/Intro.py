b92d70a0af3acfd8c4e0bba09caa72896f731729
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-01-15 16:35:18 -0500

    Create Intro.py

---
+++ Mosh/Intro.py
@@ -0,0 +1,8 @@
+# First Python coding using PyCharm
+print("This is how to print text")
+ben = "My name is ben"
+roger = "My name is roger"
+
+print(ben, roger, "This is how to use variables in a method")
+# Key takeaways from first 15 minutes or so of using Python vs Java
+# Semicolons aren't used for statements, code appears to be much shorter due to syntax style, and Python is dynamic while Java is statically typed
