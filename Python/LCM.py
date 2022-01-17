72b0b2851688a90fc5fed98d1ad1ff7bc62829c7
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-01-16 23:15:29 -0500

    Create LCM.py

---
+++ Python/LCM.py
@@ -0,0 +1,21 @@
+# find the least common denominator
+
+def lcm(x, y):
+  
+  if x > y:
+    greater = x
+  else:
+    greater = y
+    
+  while(True):
+    if((greater % x == 0) and (greater % y == 0)):
+      lcm = greater
+      break
+    greater += 1
+    
+  return lcm
+
+num1 = 54
+num2 = 24
+
+print("LCM is ", lcm(num1, num2))
