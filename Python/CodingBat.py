b9dbed96019ccaccace09b3dd5e26f7fa80009c0
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-01-21 20:52:19 -0500

    Create CodingBat.py

---
+++ Python/CodingBat.py
@@ -0,0 +1,21 @@
+# Sleep in
+def sleep_in(weekday, vacation):
+  
+  if vacation:
+    return True
+  elif weekday:
+    return False
+  else: 
+    return True
+
+# HelloName
+def hello_name(name):
+  return ("Hello " + name + "!")
+
+# MonkeyTrouble
+def monkey_trouble(a_smile, b_smile):
+  
+  if a_smile == b_smile:
+    return True
+  else:
+    return False
