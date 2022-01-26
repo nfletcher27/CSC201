c49a7cf117eb78a84147f8725e8feae6145fff9b
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-01-25 21:14:23 -0500

    Create String12.java

---
+++ CodingBat/String12.java
@@ -0,0 +1,43 @@
+// makeAbba
+public String makeAbba(String a, String b) {
+  return a+b+b+a;
+}
+
+// extraEnd
+public String extraEnd(String str) {
+  return str.substring(str.length()-2, str.length()) +
+  str.substring(str.length()-2, str.length()) +
+  str.substring(str.length()-2, str.length());
+}
+
+// makeTags
+public String makeTags(String tag, String word) {
+  return "<"+tag+">" + word + "</"+tag+">";
+}
+
+// firstTwo
+public String firstTwo(String str) {
+  if(str.length() > 2) {
+    return str.substring(0, 2);
+  } else {
+    return str;
+  }
+}
+
+// withoutEnd
+public String withoutEnd(String str) {
+  return str.substring(1, str.length()-1);
+}
+
+// withoutX2
+public String withoutX2(String str) {
+  if(str.length() > 2) {
+    if(str.substring(0, 2).contains("x")) {
+      return str.substring(0, 2).replace("x", "") + str.substring(2, str.length());
+    } else {
+      return str;
+    }
+  } else {
+    return str.replace("x", "");
+  }
+}
