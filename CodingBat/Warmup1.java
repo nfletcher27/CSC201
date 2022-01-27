ded3f00b76c93d5db4c1321a9126472aea23acf8
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-01-27 00:11:31 -0500

    Update Warmup1.java

--- CodingBat/Warmup1.java
+++ CodingBat/Warmup1.java
@@ -41,3 +41,15 @@ public boolean mixStart(String str) {
     return false;
   }
 }
+
+// endUp
+public String endUp(String str) {
+  if(str.length() < 3) {
+    return str.toUpperCase();
+  } else {
+    return str.substring(0, str.length()-3) + 
+    str.substring(str.length()-3, str.length()).toUpperCase();
+  }
+}
+
+//
