53e553fe55e5dd1bf26d072055b220d6fe9c3f78
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-01-29 18:26:04 -0500

    Update Logic2.java

--- CodingBat/Logic2.java
+++ CodingBat/Logic2.java
@@ -37,3 +37,20 @@ public int fixTeen(int n) {
     return n;
   }
 }
+
+// blackJack
+public int blackjack(int a, int b) {
+  if(a > 21 && b > 21) {
+    return 0;
+  } else if (a > 21 || b > 21) {
+    if(a > 21) {
+      return b;
+    } else {
+      return a;
+    }
+  } else if (a > b) {
+    return a;
+  } else {
+    return b;
+  }
+}
