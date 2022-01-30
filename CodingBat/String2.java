750617a7b3433df1753638f70b0d58d4026993af
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-01-29 19:00:49 -0500

    Update String2.java

--- CodingBat/String2.java
+++ CodingBat/String2.java
@@ -8,3 +8,18 @@ public int countHi(String str) {
     }
   return hiCount;
 }
+
+// bobThere
+public boolean bobThere(String str) {
+  
+  if(str.length() > 2 && !str.equals("bbc") {
+    for(int i = 0; i < str.length()-1; i++) {
+      if(str.charAt(i) == 'b' && str.charAt(i+2) == 'b') {
+        return true;
+      }
+    }
+  } else {
+    return false;
+  }
+  return false;
+}
