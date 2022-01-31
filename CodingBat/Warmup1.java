164e0d42a6e915970761c98227f6ae6a59500f0c
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-01-30 23:51:17 -0500

    Update Warmup1.java

--- CodingBat/Warmup1.java
+++ CodingBat/Warmup1.java
@@ -71,3 +71,21 @@ public boolean loneTeen(int a, int b) {
     return false;
   }
 }
+
+// stringE
+public boolean stringE(String str) {
+  int eCount = 0;
+  for(int i = 0; i < str.length(); i++) {
+    if(str.charAt(i) == 'e') {
+      eCount++;
+    } else {
+      eCount += 0;
+    }
+  }
+  
+  if(eCount > 0 && eCount < 4) {
+    return true;
+  } else {
+    return false;
+  }
+}
