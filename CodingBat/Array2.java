f79098492ced7b96017fa25d74b0dd822f570de7
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-01-31 21:30:23 -0500

    Update Array2.java

--- CodingBat/Array2.java
+++ CodingBat/Array2.java
@@ -12,3 +12,30 @@ public int[] fizzArray(int n) {
   }
   
 }
+
+// no14
+public boolean no14(int[] nums) {
+  Boolean is1 = false;
+  Boolean is4 = false;
+  for(int i = 0; i < nums.length; i++) {
+    if(nums[i] == 4) {
+      is1 = true;
+    } else if(nums[i] == 1) {
+      is4 = true;
+    } else {
+      is1 = false;
+    }
+  }
+  
+  if(is1 && is4) {
+    return false;
+  } else if(nums.length > 3) {
+    if(nums[0] == 1 && nums[3] == 4 || nums[0] == 4 && nums[3] == 1) {
+      return false;
+    } else {
+      return true;
+    }
+  } else {
+    return true;
+  }
+}
