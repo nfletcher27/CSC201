3162bd6e8da342a768d840b44cce9b794c6fbee7
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-01-31 21:19:44 -0500

    Create Array2.java

---
+++ CodingBat/Array2.java
@@ -0,0 +1,14 @@
+// fizzArray
+public int[] fizzArray(int n) {
+  int[] nums = new int[n];
+  
+  if(n != 0) {
+    for(int i = 0; i < n; i++) {
+      nums[i] = i;
+    }
+    return nums;
+  } else {
+    return nums;
+  }
+  
+}
