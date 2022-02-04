3ce42799b4b74eb05e623701601dfd4e05cfdce5
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-02-03 21:55:57 -0500

    Update Array1.java

--- CodingBat/Array1.java
+++ CodingBat/Array1.java
@@ -104,3 +104,16 @@ public int[] middleWay(int[] a, int[] b) {
   fresh[1] = b[1];
   return fresh;
 }
+
+// no23
+public boolean no23(int[] nums) {
+  
+  for(int i = 0; i < nums.length; i++) {
+    if(nums[i] == 3 || nums[i+1] == 3 || nums[i] == 2 || nums[i+1] == 2) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  return false;
+}
