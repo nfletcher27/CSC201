f54e2debbdca680cc163be3105073f5262816961
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-02-03 21:32:44 -0500

    Update Array1.java

--- CodingBat/Array1.java
+++ CodingBat/Array1.java
@@ -97,3 +97,10 @@ public int[] biggerTwo(int[] a, int[] b) {
   }
 }
 
+// middleWay
+public int[] middleWay(int[] a, int[] b) {
+  int[] fresh = new int[2];
+  fresh[0] = a[1];
+  fresh[1] = b[1];
+  return fresh;
+}
