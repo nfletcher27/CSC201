a07ca202d81e437dba0e7690a01397af2902a98f
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-02-04 23:21:36 -0500

    Update Warmup1.java

--- CodingBat/Warmup1.java
+++ CodingBat/Warmup1.java
@@ -149,3 +149,20 @@ public String everyNth(String str, int n) {
     }
   return finished;
 }
+
+// startOz
+public String startOz(String str) {
+ if(str.length() < 2) {
+   return str;
+ } else {
+    if(str.substring(0, 2).equals("oz")) {
+    return "oz";
+  } else if(str.charAt(0) == 'o') {
+    return "o";
+  } else if(str.charAt(1) == 'z') {
+    return "z";
+  } else {
+    return "";
+  }
+ }
+}
