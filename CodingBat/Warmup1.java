1b249827205b4569a4ab38f90c5f878579a3aee3
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-02-04 23:46:09 -0500

    Update Warmup1.java

--- CodingBat/Warmup1.java
+++ CodingBat/Warmup1.java
@@ -175,3 +175,20 @@ public boolean lastDigit(int a, int b) {
     return false;
 }
 }
+
+// idek
+public int max1020(int a, int b) {
+  if(!(a > 9 && a < 21) && !(b > 9 && b < 21)) {
+    return 0;
+  } else if((a > 9 && a < 21) && (b > 9 && b < 21)) {
+    if(a > b) {
+      return a;
+    } else {
+      return b;
+    }
+  } else if(a > 9 && a < 21) {
+    return a;
+  } else {
+    return b;
+  }
+}
