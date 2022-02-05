4864459bd4c9473020a50059782ec6ab0d50ed23
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-02-04 23:23:52 -0500

    Update Warmup1.java

--- CodingBat/Warmup1.java
+++ CodingBat/Warmup1.java
@@ -166,3 +166,12 @@ public String startOz(String str) {
   }
  }
 }
+
+// lastDigit
+public boolean lastDigit(int a, int b) {
+  if(a % 10 == b % 10) {
+    return true;
+  } else {
+    return false;
+}
+}
