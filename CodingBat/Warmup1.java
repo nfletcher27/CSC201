3996d7a4f4158f85a8cffd30cf5b154222f9f7ff
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-02-04 22:31:13 -0500

    Update Warmup1.java

--- CodingBat/Warmup1.java
+++ CodingBat/Warmup1.java
@@ -118,3 +118,15 @@ public boolean in3050(int a, int b) {
     return false;
   }
 }
+
+// delDel
+public String delDel(String str) {
+  if(str.length() < 4) {
+    return str;
+  } else if(str.substring(1, 4).equals("del")) {
+    return str.substring(0, 1) + str.substring(4, str.length());
+  } else {
+    return str;
+  }
+}
+
