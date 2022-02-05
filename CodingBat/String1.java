92f22dd5679bf9277700f0c727dbce57a8cdf676
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-02-04 22:33:53 -0500

    Update String1.java

--- CodingBat/String1.java
+++ CodingBat/String1.java
@@ -49,3 +49,12 @@ public String withoutX(String str) {
 	     return str;
 	   }
 }
+
+// the end
+public String theEnd(String str, boolean front) {
+  if(front) {
+    return str.substring(0, 1);
+  } else {
+    return str.substring(str.length()-1, str.length());
+  }
+}
