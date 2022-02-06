dcc9e4090df77fa7013cd913876b6e887e70f6aa
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-02-05 22:50:12 -0500

    Update Array1.java

--- CodingBat/Array1.java
+++ CodingBat/Array1.java
@@ -141,3 +141,13 @@ public int[] make2(int[] a, int[] b) {
   }
   return fresh;
 }
+
+// fix23
+public int[] fix23(int[] nums) {
+  if(nums[0] == 2 && nums[1] == 3) {
+    nums[1] = 0;
+  } else if(nums[1] == 2 && nums[2] == 3) {
+    nums[2] = 0;
+  } 
+  return nums;
+}
