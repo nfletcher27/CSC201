55dce29a35dbdc72b0474c8c11046f8054e03637
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-02-12 23:56:38 -0500

    Update Logic1.java

--- CodingBat/Logic1.java
+++ CodingBat/Logic1.java
@@ -195,3 +195,10 @@ public boolean isTeen(int a) {
     return false;
   }
 }
+
+// dateFashion
+public int dateFashion(int you, int date) {
+  if(you <= 2 || date <= 2) return 0;
+  if(you >= 8 || date >= 8) return 2;
+  return 1;
+}
