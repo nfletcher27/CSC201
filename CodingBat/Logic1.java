840394f9c5006ef06ce1a56f9b016f4504a3b46b
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-02-13 22:32:02 -0500

    Update Logic1.java

--- CodingBat/Logic1.java
+++ CodingBat/Logic1.java
@@ -209,3 +209,8 @@ public boolean more20(int n) {
   return (((n%20) >= 1 || (n%20) >= 2) && n%20 < 3);
 }
 
+// twoAsOne
+public boolean twoAsOne(int a, int b, int c) {
+  if(a+b==c||b+c==a||c+a==b) return true;
+  return false;
+}
