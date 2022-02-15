671a0e903317f724681f8a9d4c68cf0f79dafe87
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-02-15 16:58:55 -0500

    Update AP1.java

--- CodingBat/AP1.java
+++ CodingBat/AP1.java
@@ -22,3 +22,13 @@ public boolean scoresIncreasing(int[] scores) {
   return true;
 }
 
+// scoresClump
+public boolean scoresClump(int[] scores) {
+  for(int i = 0; i < scores.length-2; i++) {
+    if(Math.abs(scores[i]-(scores[i+1])) <= 2 && Math.abs(scores[i]-(scores[i+2]))
+    <= 2 && Math.abs((scores[i+1])-(scores[i+2])) <= 2) {
+      return true;
+    }
+  }
+  return false;
+}
