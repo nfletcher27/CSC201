26a1834722d91e5c4615322a2d79de0c78d1cf78
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-02-15 00:39:21 -0500

    Update String1.java

--- CodingBat/String1.java
+++ CodingBat/String1.java
@@ -78,3 +78,9 @@ public String atFirst(String str) {
   if(str.length() > 0) return str.substring(0, 1) + "@";
   return "@@";
 }
+
+// extraFront
+public String extraFront(String str) {
+  if(str.length() < 2) return str + str + str;
+  return str.substring(0, 2) + str.substring(0, 2) + str.substring(0, 2);
+}
