19759cf400d091185b0d8428d020f60aef57d33f
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-02-19 23:48:26 -0500

    Update String1.java

--- CodingBat/String1.java
+++ CodingBat/String1.java
@@ -125,3 +125,8 @@ public boolean frontAgain(String str) {
   if(str.length() < 2) return false;
   return (str.substring(0,2).equals(str.substring(str.length()-2, str.length())));
 }
+
+// left2
+public String left2(String str) {
+  return str.substring(2, str.length()) + str.substring(0,2);
+}
