1f34be679ef225a8ee90b1ea87bed46656b39d0c
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-03-05 19:45:33 -0500

    Update Warmup2.java

--- CodingBat/Warmup2.java
+++ CodingBat/Warmup2.java
@@ -126,3 +126,12 @@ public String stringBits(String str) {
   }
   return newS;
 }
+
+// arrayCount9
+public int arrayCount9(int[] nums) {
+  int count = 0;
+  for(int i = 0; i < nums.length; i++) {
+    if(nums[i] == 9) count++;
+  }
+  return count;
+}
