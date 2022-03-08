f51fe74a1ec8a79f25dad446557e0a0ead292dcb
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-03-08 17:25:33 -0500

    Update Logic1.java

--- CodingBat/Logic1.java
+++ CodingBat/Logic1.java
@@ -259,3 +259,9 @@ public int redTicket(int a, int b, int c) {
   return 0;
 }
 
+// blueTicket
+public int blueTicket(int a, int b, int c) {
+  if(a+b == 10 || b+c == 10 || a+c == 10) return 10;
+  if(a+b == b+c+10 || a+b == a+c+10) return 5;
+  return 0;
+}
