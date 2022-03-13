fada7a05e3dc726696a59ff2e4600a46d1980700
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-03-13 19:47:07 -0400

    Update Array1.java

--- CodingBat/Array1.java
+++ CodingBat/Array1.java
@@ -216,3 +216,15 @@ public int[] swapEnds(int[] nums) {
   }
   return arr;
 }
+
+// double23
+public boolean double23(int[] nums) {
+  int count2 = 0;
+  int count3 = 0;
+  for(int i = 0; i < nums.length; i++) {
+    if(nums[i] == 2) count2++;
+    if(nums[i] == 3) count3++;
+  }
+  if(count2 > 1 || count3 > 1) return true;
+  return false;
+}
