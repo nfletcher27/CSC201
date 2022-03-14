936ed684fe8d5ce488484d7c2097e612dc8c0574
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-03-14 00:36:27 -0400

    Update Array2.java

--- CodingBat/Array2.java
+++ CodingBat/Array2.java
@@ -149,3 +149,15 @@ public boolean lucky13(int[] nums) {
   }
   return (!saw1 && !saw3);
 }
+
+// modThree
+public boolean modThree(int[] nums) {
+  if(nums.length >= 3) {
+    for(int i = 0; i < nums.length-2; i++) {
+      if(nums[i] % 2 == 0 && nums[i+1] % 2 == 0 && nums[i+2] % 2 == 0) return true;
+      if(nums[i] % 2 != 0 && nums[i+1] % 2 != 0 && nums[i+2] % 2 != 0) return true;
+    }
+    return false;
+  }
+  return false;
+}
