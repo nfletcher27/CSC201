c1dcb7dd37f7c5021fa6518fa8d24a54a61c8e25
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-03-14 00:27:44 -0400

    Update Warmup2.java

--- CodingBat/Warmup2.java
+++ CodingBat/Warmup2.java
@@ -169,3 +169,15 @@ public String frontTimes(String str, int n) {
   }
   return part;
 }
+
+// array123
+public boolean array123(int[] nums) {
+  if(nums.length >= 3) {
+    for(int i = 0; i < nums.length-1; i++) {
+      if(nums[i] == 1 && nums[i+1] == 2 && nums[i+2] == 3) return true;
+    }
+    return false;
+  }
+  return false;
+}
+
