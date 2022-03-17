23e1fe78b5cb4446630fa41874a41b8df24729c5
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-03-16 22:38:23 -0400

    Update Logic1.java

--- CodingBat/Logic1.java
+++ CodingBat/Logic1.java
@@ -271,3 +271,9 @@ public boolean lessBy10(int a, int b, int c) {
   return (a >= b + 10 || b >= c + 10 || c >= a + 10 || c >= b + 10);
 }
 
+// sumLimit
+public int sumLimit(int a, int b) {
+  int total = a + b;
+  if(String.valueOf(total).length() == String.valueOf(a).length()) return total;
+  return a;
+}
