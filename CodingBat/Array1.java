c9a28fbd8b88de0e051e9d8eda33f79b717adf97
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-03-20 21:29:49 -0400

    Update Array1.java

--- CodingBat/Array1.java
+++ CodingBat/Array1.java
@@ -228,3 +228,13 @@ public boolean double23(int[] nums) {
   if(count2 > 1 || count3 > 1) return true;
   return false;
 }
+
+// plusTwo
+public int[] plusTwo(int[] a, int[] b) {
+  int[] c = new int[4];
+  c[0] = a[0];
+  c[1] = a[1];
+  c[2] = b[0];
+  c[3] = b[1];
+  return c;
+}
