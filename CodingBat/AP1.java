59b9bd15fbec31dc344843ef5e4223910623812e
Nesta Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-03-29 00:06:40 -0400

    Update AP1.java

--- CodingBat/AP1.java
+++ CodingBat/AP1.java
@@ -72,3 +72,12 @@ public List wordsWithoutList(String[] words, int len) {
   }
   return without;
 }
+
+// sumHeights
+public int sumHeights(int[] heights, int start, int end) {
+  int counter = 0;
+  for(int i = start; i < end; i++) {
+    counter += Math.abs(heights[i]-heights[i+1]);
+  }
+  return counter;
+}
