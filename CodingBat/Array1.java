013eff2f80dd4d3b4d1cd2c525ad7407cc78eaea
Nesta R. Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-04-06 15:15:20 -0400

    Update Array1.java

--- CodingBat/Array1.java
+++ CodingBat/Array1.java
@@ -248,3 +248,12 @@ public int[] midThree(int[] nums) {
   midT[2] = nums[mid+1];
   return midT;
 }
+
+// rorateLeft3
+public int[] rotateLeft3(int[] nums) {
+  int temp = nums[0];
+  nums[0] = nums[1];
+  nums[1] = nums[2];
+  nums[2] = temp;
+  return nums;
+}
