5b70e2610565d7faab63e006998a320aad5bb65f
Nesta R. Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-04-14 22:06:08 -0400

    Update Array1.java

--- CodingBat/Array1.java
+++ CodingBat/Array1.java
@@ -274,3 +274,11 @@ public boolean unlucky1(int[] nums) {
   if (nums.length > 2 && nums[nums.length-2] == 1 && nums[nums.length-1] == 3) return true;
   return false;
 }
+
+// makeMiddle
+public int[] makeMiddle(int[] nums) {
+  int[] nums2 = new int[2];
+  nums2[0] = nums[nums.length/2-1];
+  nums2[1] = nums[nums.length/2];
+  return nums2;
+}
