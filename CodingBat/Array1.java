1eae301b2aba411748a9b86f77c5a7ef5d55d5aa
Nesta R. Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-04-14 22:03:06 -0400

    Update Array1.java

--- CodingBat/Array1.java
+++ CodingBat/Array1.java
@@ -266,3 +266,11 @@ public int start1(int[] a, int[] b) {
   return 0;
 }
 
+// unlucky1 
+public boolean unlucky1(int[] nums) {
+  if (nums.length > 1 && nums[0] == 1 && nums[1] == 3) return true;
+  if (nums.length > 1 && nums[1] == 1 && nums[2] == 3) return true;
+  if (nums.length > 2 && nums[nums.length-2] == 1 && nums[nums.length-2] == 3) return true;
+  if (nums.length > 2 && nums[nums.length-2] == 1 && nums[nums.length-1] == 3) return true;
+  return false;
+}
