f6cdea306c6bf3f783113cce3f17c1e60b3ca034
Nesta R. Fletcher <89424122+nfletcher27@users.noreply.github.com>
2022-04-20 21:20:21 -0400

    Update String2.java

--- CodingBat/String2.java
+++ CodingBat/String2.java
@@ -108,3 +108,13 @@ public boolean prefixAgain(String str, int n) {
   return false;
 }
 
+// sameStarChar
+public boolean sameStarChar(String str) {
+  for(int i = 0; i < str.length(); i++) {
+    if(str.charAt(i) == ('*')) {
+      if(i == 0 || i == str.length()-1) continue;
+      if(str.charAt(i-1) != str.charAt(i+1)) return false;
+    }
+  }
+  return true;
+}
