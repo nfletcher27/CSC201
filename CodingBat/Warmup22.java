4acc01ffc25159c108196f8c6faecf0699c98240
Nesta <89424122+nfletcher27@users.noreply.github.com>
2022-05-02 19:50:44 -0400

    Update Warmup22.java

--- CodingBat/Warmup22.java
+++ CodingBat/Warmup22.java
@@ -28,3 +28,15 @@ int countXX(String str) {
   }
   return xxCount;
 }
+
+// last2
+public int last2(String str) {
+  if(str.length() < 2) return 0;
+  String st = str.substring(str.length()-2, str.length());
+  int counter = 0;
+  for(int i = 0; i < str.length()-1; i++) {
+    if(str.substring(i, i+2).equals(st)) counter++;
+  }
+  if(counter < 1) return 0;
+  return counter-1;
+}
